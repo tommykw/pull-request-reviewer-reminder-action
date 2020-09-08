e7f94866ee8ef6403fc74813c660e56c1a2cdb6e
tommykw <destinys.lotus.1108mn@gmail.com>
2020-09-09 08:53:56 +0900

    debug test

--- src/main.ts
+++ src/main.ts
@@ -12,43 +12,43 @@ async function run(): Promise<void> {
 
     core.info(`pr num: ${pullRequests.length}`)
 
-    core.info(`pr info: ${JSON.stringify(pullRequests)}`)
+    //core.info(`pr info: ${JSON.stringify(pullRequests)}`)
 
-    const reviewCommentUrls = pullRequests.map(pr => {
-      pr.review_comments_url
-    })
+    // const reviewCommentUrls = pullRequests.map(pr => {
+    //   pr.review_comments_url
+    // })
 
-    const commentUrls = pullRequests.map(pr => {
-      pr.comments_url
-    })
+    // const commentUrls = pullRequests.map(pr => {
+    //   pr.comments_url
+    // })
 
-    const createdList = pullRequests.map(pr => {
-      pr.created_at
-    })
+    // const createdList = pullRequests.map(pr => {
+    //   pr.created_at
+    // })
 
-    const titles = pullRequests.map(pr => {
-      pr.title
-    })
+    // const titles = pullRequests.map(pr => {
+    //   pr.title
+    // })
 
-    const bodies = pullRequests.map(pr => {
-      pr.body
-    })
+    // const bodies = pullRequests.map(pr => {
+    //   pr.body
+    // })
 
     const prIds = pullRequests.map(pr => {
       pr.id
     })
 
-    const states = pullRequests.map(pr => {
-      pr.state
-    })
+    // const states = pullRequests.map(pr => {
+    //   pr.state
+    // })
 
-    core.info(`pr comment review : ${reviewCommentUrls}`)
-    core.info(`pr comment : ${commentUrls}`)
-    core.info(`pr created : ${createdList}`)
-    core.info(`pr title : ${titles}`)
-    core.info(`pr bodies : ${bodies}`)
+    // core.info(`pr comment review : ${reviewCommentUrls}`)
+    // core.info(`pr comment : ${commentUrls}`)
+    // core.info(`pr created : ${createdList}`)
+    // core.info(`pr title : ${titles}`)
+    // core.info(`pr bodies : ${bodies}`)
     core.info(`pr ids : ${prIds}`)
-    core.info(`pr states : ${states}`)
+    // core.info(`pr states : ${states}`)
     // eslint-disable-next-line no-empty
   } catch (error) {}
 }
