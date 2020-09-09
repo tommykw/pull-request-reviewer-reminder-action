1bcb50e1267d0aff88ca8ebd95f0caf50fd2f741
tommykw <destinys.lotus.1108mn@gmail.com>
2020-09-09 12:38:54 +0900

    debug test

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,7 @@ async function run(): Promise<void> {
       })
 
       core.info(`created ${pr.created_at}`)
+      core.info(`created ${pr.created_at}`)
 
       if (prInfo.requested_reviewers.length === 0) {
         continue
